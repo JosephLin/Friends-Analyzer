e0a6e9354b2082acdbcbbe8a4103da8b6193e108
Joseph Lin <joseph@eros.ny.rga.com>
2011-03-08 17:44:40 -0500

    test

---
+++ Classes/Entity.h
@@ -0,0 +1,17 @@
+//
+//  Entity.h
+//  FriendsAnalyzer
+//
+//  Created by Joseph Lin on 3/8/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Entity : NSManagedObject {
+@private
+}
+
+@end
---
+++ Classes/Entity.m
@@ -0,0 +1,14 @@
+//
+//  Entity.m
+//  FriendsAnalyzer
+//
+//  Created by Joseph Lin on 3/8/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Entity.h"
+
+
+@implementation Entity
+
+@end

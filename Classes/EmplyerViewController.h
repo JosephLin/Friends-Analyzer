9014f16574e9d25407e01343d579a75d1efe10aa
Joseph Lin <joseph@eros.ny.rga.com>
2011-03-14 16:31:58 -0400

    xxx

---
+++ Classes/EmplyerViewController.h
@@ -0,0 +1,18 @@
+//
+//  EmplyerViewController.h
+//  FriendsAnalyzer
+//
+//  Created by Joseph Lin on 3/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FRCBasedTableViewController.h"
+
+
+@interface EmplyerViewController : FRCBasedTableViewController
+{
+    
+}
+
+@end

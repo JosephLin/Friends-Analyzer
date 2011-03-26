05a8407fdd61f26604557c19ee16ab08860923f6
Joseph Lin <Joseph@joseph-pc.home>
2011-03-26 19:09:53 -0400

    Pie Chart

--- Classes/PieChartView.m
+++ Classes/PieChartView.m
@@ -9,6 +9,13 @@
 #import "PieChartView.h"
 #import "UIColor-Expanded.h"
 
+#define kLabelSquareSize        10.0
+#define kLabelSquareSpacing     5.0
+#define kLabelFontSize          12.0
+#define kLabelRowHeight         15.0
+
+
+
 @implementation PieChartView
 
 @synthesize dict, colorScheme;
@@ -22,6 +29,13 @@
         self.backgroundColor = [UIColor whiteColor];
     }
     return self;
+}
+
+- (void)dealloc
+{
+	[dict release];
+	[colorScheme release];
+    [super dealloc];
 }
 
 - (void)setFrame:(CGRect)frame
@@ -38,135 +52,6 @@
     [self setNeedsDisplay];
 }
 
-- (void)drawRect:(CGRect)rect
-{
-    CGContextRef context = UIGraphicsGetCurrentContext();
-	
-	UIGraphicsPushContext(context);
-    {
-        //// Draw Background Gradient ////
-        CGFloat locations[3] = { 0.0, 0.8, 1.0 };
-        CGFloat components[12] = { 
-            1.0, 1.0, 1.0, 1.0,
-            0.8, 0.8, 0.8, 1.0,
-            0.7, 0.7, 0.7, 1.0
-        };
-        
-        CGColorSpaceRef rgbColorspace = CGColorSpaceCreateDeviceRGB();
-        CGGradientRef glossGradient = CGGradientCreateWithColorComponents(rgbColorspace, components, locations, 3);
-        
-        CGRect currentBounds = self.bounds;
-        CGPoint topCenter = CGPointMake( CGRectGetMidX(currentBounds), 0.0f);
-        CGPoint bottomCenter = CGPointMake( CGRectGetMidX(currentBounds), CGRectGetHeight(currentBounds));
-        CGContextDrawLinearGradient(context, glossGradient, topCenter, bottomCenter, 0);
-        
-        CGGradientRelease(glossGradient);
-        CGColorSpaceRelease(rgbColorspace);
-        
-        
-        NSNumber* valueSum = [[dict allValues] valueForKeyPath:@"@sum.intValue"];
-        NSInteger total = [valueSum intValue];
-        CGFloat startValue = 0;
-        int colorIndex = 0;
-
-        CGFloat radius = ( MIN( rect.size.width , rect.size.height ) / 2 ) * 0.8;
-        CGFloat centerX = CGRectGetMidX(rect);
-        CGFloat centerY = CGRectGetMidY(rect);
-        
-        CGFloat labelX = 15.0;
-        CGFloat labelY = 15.0;
-        
-        NSArray* sortedKeys = [[dict allKeys] sortedArrayUsingComparator:(NSComparator)^(id obj1, id obj2){
-            if ( [obj1 intValue] )
-            {
-                return [obj1 intValue] > [obj2 intValue];
-            }
-            else
-            {
-                return [obj1 caseInsensitiveCompare:obj2];
-            }
-        }];
-        for ( NSString* key in sortedKeys )
-        {
-            NSInteger value = [[dict objectForKey:key] intValue];
-            
-            NSInteger index = colorIndex % [self.colorScheme count];
-            CGFloat k = 1 + 0.1 * colorIndex / [self.colorScheme count];
-            colorIndex++;
-            
-            NSDictionary* colorDict = [self.colorScheme objectAtIndex:index];
-            CGFloat red = [[colorDict objectForKey:@"red"] floatValue] * k;
-            CGFloat green = [[colorDict objectForKey:@"green"] floatValue] * k;
-            CGFloat blue = [[colorDict objectForKey:@"blue"] floatValue] * k;
-            
-			UIColor* color = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
-			
-//            UIColor* color = [self.colorScheme objectAtIndex:colorIndex];
-            [color setStroke];
-            [color setFill];
-//            colorIndex = ( colorIndex + 1 ) % [colorScheme count];
-            
-            CGFloat endValue = startValue + value;
-            CGFloat startAngle = 2 * M_PI * startValue / total;
-            CGFloat endAngle = 2 * M_PI * endValue / total;
-            
-            // Draw pie
-            CGContextAddArc(context, centerX, centerY, radius, startAngle - M_PI_2, endAngle - M_PI_2, NO);
-            CGContextAddLineToPoint(context, centerX, centerY);
-            CGContextDrawPath(context, kCGPathFill);
-          
-            CGContextClosePath(context);
-/*
-            CGRect rect = CGContextGetPathBoundingBox(context);
-            CGContextSaveGState(context);
-            CGContextClip(context);
-
-
-            CGFloat locations[2] = { 0.0, 1.0 };
-            CGFloat components[8] = { 
-                color.red, color.green, color.blue, 1.0,
-                color.red * 0.5, color.green * 0.5, color.blue * 0.5, 1.0
-            };
-            
-            CGColorSpaceRef rgbColorspace = CGColorSpaceCreateDeviceRGB();
-            CGGradientRef glossGradient = CGGradientCreateWithColorComponents(rgbColorspace, components, locations, 2);
-            
-            
-            CGPoint topCenter = rect.origin;
-            CGPoint bottomCenter = CGPointMake(rect.origin.x + rect.size.width, rect.origin.y + rect.size.height);
-            CGContextDrawLinearGradient(context, glossGradient, topCenter, bottomCenter, 0);
-            
-            CGGradientRelease(glossGradient);
-            CGColorSpaceRelease(rgbColorspace);
-            
-            CGContextRestoreGState(context);
-*/
-            
-            // Draw Label
-            CGRect squareFrame = CGRectMake(labelX, labelY, 10.0, 10.0);
-            CGContextFillRect(context, squareFrame);
-            
-            [[UIColor blackColor] setFill];
-
-            CGRect labelFrame = CGRectMake(labelX + 15.0, labelY, 200.0, 15.0);
-            [key drawInRect:labelFrame withFont:[UIFont systemFontOfSize:12.0]];
-            
-            
-            // Set next start point
-            startValue = endValue;
-            labelY += 20.0;
-        }
-    }
-	UIGraphicsPopContext();
-}
-
-- (void)dealloc
-{
-	[dict release];
-	[colorScheme release];
-    [super dealloc];
-}
-
 - (NSArray*)colorScheme
 {
 	if ( !colorScheme )
@@ -178,25 +63,206 @@
 	return colorScheme;
 }
 
-/*
-- (NSArray*)colorScheme
+- (NSArray*)sortedKeys
 {
-	if ( !colorScheme )
-	{
-		NSString* plistPath = [[NSBundle mainBundle] pathForResource:@"PieChartColorScheme" ofType:@"plist"];
-		NSArray* plistArray = [NSArray arrayWithContentsOfFile:plistPath];
-		NSMutableArray* tempArray = [NSMutableArray arrayWithCapacity:[plistArray count]];
-		
-		for ( NSString* hexString in plistArray )
-		{
-			UIColor* color = [UIColor colorWithHexString:hexString];
-			[tempArray addObject:color];
-		}
-		colorScheme = [[NSArray alloc] initWithArray:tempArray];
-	}
-	return colorScheme;
+    return [[dict allKeys] sortedArrayUsingComparator:(NSComparator)^(id obj1, id obj2){
+        if ( [obj1 intValue] )
+        {
+            return [obj1 intValue] > [obj2 intValue];
+        }
+        else
+        {
+            return [obj1 caseInsensitiveCompare:obj2];
+        }
+    }];
 }
-*/
+
+- (UIColor*)colorAtIndex:(NSInteger)index
+{
+    NSInteger colorIndex = index % [self.colorScheme count];
+    CGFloat colorFactor = 1 + 0.1 * index / [self.colorScheme count];
+    
+    NSDictionary* colorDict = [self.colorScheme objectAtIndex:colorIndex];
+    CGFloat red = [[colorDict objectForKey:@"red"] floatValue] * colorFactor;
+    CGFloat green = [[colorDict objectForKey:@"green"] floatValue] * colorFactor;
+    CGFloat blue = [[colorDict objectForKey:@"blue"] floatValue] * colorFactor;
+    
+    return [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+}
+
+
+
+#pragma mark -
+#pragma mark Drawing
+
+- (void)drawBackground
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+	
+    CGFloat locations[3] = { 0.0, 0.8, 1.0 };
+    CGFloat components[12] = { 
+        1.0, 1.0, 1.0, 1.0,
+        0.8, 0.8, 0.8, 1.0,
+        0.7, 0.7, 0.7, 1.0
+    };
+    
+    CGColorSpaceRef rgbColorspace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef glossGradient = CGGradientCreateWithColorComponents(rgbColorspace, components, locations, 3);
+    
+    CGRect currentBounds = self.bounds;
+    CGPoint topCenter = CGPointMake( CGRectGetMidX(currentBounds), 0.0f);
+    CGPoint bottomCenter = CGPointMake( CGRectGetMidX(currentBounds), CGRectGetHeight(currentBounds));
+    CGContextDrawLinearGradient(context, glossGradient, topCenter, bottomCenter, 0);
+    
+    CGGradientRelease(glossGradient);
+    CGColorSpaceRelease(rgbColorspace);
+}
+
+- (void)drawLabels:(NSArray*)values inRect:(CGRect)rect
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    int index = 0;
+
+    NSArray* sortedKeys = [self sortedKeys];
+    NSNumber* maxKeyLength = [sortedKeys valueForKeyPath:@"@max.length"];
+    CGFloat labelWidth = [maxKeyLength intValue] * 20.0 + kLabelSquareSize;
+    NSInteger labelColCount;
+    NSInteger labelRowCount;
+    if ( rect.size.width > rect.size.height )
+    {
+        labelColCount = rect.size.width / labelWidth;
+        labelRowCount = ceilf( (float)[sortedKeys count] / labelColCount );
+    }
+    else
+    {
+        labelRowCount = rect.size.height / kLabelRowHeight;
+        labelColCount = ceilf( (float)[sortedKeys count] / labelRowCount );        
+    }
+    
+    for ( NSString* key in [self sortedKeys] )
+    {
+        //// Select Color ////
+        UIColor* color = [self colorAtIndex:index];
+        [color setStroke];
+        [color setFill];
+        index++;
+        
+        //// Draw Label ////
+        if ( rect.size.width > rect.size.height )     // Portrait; draw labels at the bottom.
+        {
+            CGFloat labelX = index % labelColCount * labelWidth;
+            CGFloat labelY = index / labelColCount * kLabelRowHeight;
+            CGRect squareFrame = CGRectMake(labelX, labelY, kLabelSquareSize, kLabelSquareSize);
+            CGContextFillRect(context, squareFrame);
+            
+            [[UIColor blackColor] setFill];
+            CGRect labelFrame = CGRectMake(labelX + kLabelSquareSize + kLabelSquareSpacing, labelY, labelWidth, kLabelRowHeight);
+            [key drawInRect:labelFrame withFont:[UIFont systemFontOfSize:kLabelFontSize]];
+        }
+        else                                                        // Landscapel draw labels at the left side.
+        {
+            //            CGRect squareFrame = CGRectMake(labelX, labelY, kLabelSquareSize, kLabelSquareSize);
+            //            CGContextFillRect(context, squareFrame);
+            //            
+            //            [[UIColor blackColor] setFill];
+            //            CGRect labelFrame = CGRectMake(labelX + kLabelSquareSize + kLabelSquareSpacing, labelY, 200.0, kLabelRowHeight);
+            //            [key drawInRect:labelFrame withFont:[UIFont systemFontOfSize:kLabelFontSize]];
+        }
+    }
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+	
+    [self drawBackground];
+    
+    //// Set Dimensions ////
+    CGFloat radius = ( MIN( rect.size.width , rect.size.height ) / 2 ) * 0.8;
+    CGFloat centerX = CGRectGetMidX(rect);
+    CGFloat centerY = CGRectGetMidY(rect);
+    
+    
+    //// Set Data ////
+    NSNumber* valueSum = [[dict allValues] valueForKeyPath:@"@sum.intValue"];
+    NSInteger total = [valueSum intValue];
+    CGFloat startValue = 0;
+    int index = 0;
+    
+    NSArray* sortedKeys = [self sortedKeys];
+    
+    //    NSNumber* maxKeyLength = [sortedKeys valueForKeyPath:@"@max.length"];
+    //    CGFloat labelWidth = [maxKeyLength intValue] * 20.0 + kLabelSquareSize;
+    CGFloat labelWidth = (self.bounds.size.width - 20) / 6;
+    for ( NSString* key in sortedKeys )
+    {
+        CGSize size = [key sizeWithFont:[UIFont systemFontOfSize:kLabelFontSize]];
+        if ( size.width + kLabelSquareSize + 4 * kLabelSquareSpacing > labelWidth )
+            labelWidth = size.width + kLabelSquareSize + 4 * kLabelSquareSpacing;
+    }
+    
+    for ( NSString* key in sortedKeys )
+    {
+        //// Select Color ////
+        UIColor* color = [self colorAtIndex:index];
+        [color setStroke];
+        [color setFill];
+
+
+        //// Calculate Value ////
+        NSInteger value = [[dict objectForKey:key] intValue];
+        CGFloat endValue = startValue + value;
+        CGFloat startAngle = 2 * M_PI * startValue / total;
+        CGFloat endAngle = 2 * M_PI * endValue / total;
+        
+        
+        //// Draw pie ////
+        CGContextAddArc(context, centerX, centerY, radius, startAngle - M_PI_2, endAngle - M_PI_2, NO);
+        CGContextAddLineToPoint(context, centerX, centerY);
+        CGContextClosePath(context);
+        CGContextDrawPath(context, kCGPathFill);
+
+        
+        //// Draw Label ////
+        if ( self.bounds.size.height > self.bounds.size.width )     // Portrait; draw labels at the bottom.
+        {
+            CGRect labelRect = CGRectMake( 0, 0, self.bounds.size.width, (self.bounds.size.height - self.bounds.size.width) / 2 );
+            CGRect labelRectInset = CGRectInset(labelRect, 10, 10);
+
+            NSInteger labelColCount = labelRectInset.size.width / labelWidth;
+            CGFloat labelX = index % labelColCount * labelWidth + labelRectInset.origin.x;
+            CGFloat labelY = index / labelColCount * kLabelRowHeight + labelRectInset.origin.y;
+            CGRect squareFrame = CGRectMake(labelX, labelY, kLabelSquareSize, kLabelSquareSize);
+            CGContextFillRect(context, squareFrame);
+            
+            [[UIColor blackColor] setFill];
+            CGRect labelFrame = CGRectMake(labelX + kLabelSquareSize + kLabelSquareSpacing, labelY, labelWidth, kLabelRowHeight);
+            [key drawInRect:labelFrame withFont:[UIFont systemFontOfSize:kLabelFontSize]];
+        }
+        else                                                        // Landscapel draw labels at the left side.
+        {
+            CGRect labelRect = CGRectMake( 0, 0, (self.bounds.size.width - self.bounds.size.height) / 2, self.bounds.size.height );
+            CGRect labelRectInset = CGRectInset(labelRect, 10, 10);
+            
+            NSInteger labelRowCount = labelRectInset.size.height / kLabelRowHeight;
+            CGFloat labelX = index / labelRowCount * labelWidth + labelRectInset.origin.x;
+            CGFloat labelY = index % labelRowCount * kLabelRowHeight + labelRectInset.origin.y;
+            CGRect squareFrame = CGRectMake(labelX, labelY, kLabelSquareSize, kLabelSquareSize);
+            CGContextFillRect(context, squareFrame);
+            
+            [[UIColor blackColor] setFill];
+            CGRect labelFrame = CGRectMake(labelX + kLabelSquareSize + kLabelSquareSpacing, labelY, labelWidth, kLabelRowHeight);
+            [key drawInRect:labelFrame withFont:[UIFont systemFontOfSize:kLabelFontSize]];
+        }
+        
+        
+        //// Set next start-point ////
+        startValue = endValue;
+        index++;
+    }
+    
+}
+
 
 @end
 
